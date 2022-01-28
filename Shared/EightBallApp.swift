1eeafe264c458c3d8ae26eb85cb74b04e63201c6
Serhiy Kostrytsya <s.kostrytsya@uds.systems>
2022-01-28 13:17:20 +0200

    Initial Commit

---
+++ Shared/EightBallApp.swift
@@ -0,0 +1,17 @@
+//
+//  EightBallApp.swift
+//  Shared
+//
+//  Created by Сергій Костриця on 28.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct EightBallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
